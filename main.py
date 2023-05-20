20410466c976b70eaf0ca93abdc68bc8908a2cff
zack-fisher <zackfisher27@proton.me>
2023-05-20 03:56:13 -0400

    full project search

--- main.py
+++ main.py
@@ -19,4 +19,19 @@ if __name__ == "__main__":
         print("Usage: python lint_tscn.py <filename>")
         sys.exit(1)
 
-    lint_tscn_file(os.path.abspath(sys.argv[1]))
+    # if endswith tscn, just lint the file.
+    if sys.argv[1].endswith(".tscn"):
+        lint_tscn_file(os.path.abspath(sys.argv[1]))
+        sys.exit(0)
+    # if it's the project.godot, lint all tscn files recursively.
+    elif sys.argv[1].endswith("project.godot"):
+        project_root = res_search.find_project_root(os.path.abspath(sys.argv[1]))
+        if project_root is None:
+            error_print("Could not find the Godot project root.")
+            sys.exit(1)
+        for root, dirs, files in os.walk(project_root):
+            # get the abspath
+            for file in files:
+                if file.endswith(".tscn"):
+                    lint_tscn_file(os.path.join(root, file))
+        sys.exit(0)
